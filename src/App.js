5f3f83c014b1294532446fc218c8b3b3d8092a71
LuisEnMarroquin <mluis651@gmail.com>
2022-07-10 20:06:55 -0500

    Added table sorted by time to arrive

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import {
   Input,
   SkeletonText,
   TableContainer,
-  Text, 
-  Table, 
+  Text,
+  Table,
   Thead,
   Th,
   Tbody,
   Tr,
   Td,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
 
 import {
   useJsApiLoader,
@@ -24,50 +24,71 @@ import {
   Marker,
   Autocomplete,
   DirectionsRenderer,
-} from '@react-google-maps/api'
-import { useRef, useState } from 'react'
+} from "@react-google-maps/api";
+import { useRef, useState } from "react";
 
-const center = { lat: 25.67145219760651, lng: -100.31809720508832 }
+const center = { lat: 25.67145219760651, lng: -100.31809720508832 };
 
 function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: ['places'],
-  })
+    libraries: ["places"],
+  });
 
-  const [map, setMap] = useState(/** @type google.maps.Map */(null))
-  const [directionsResponse, setDirectionsResponse] = useState(null)
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
-  const [tableData, settableData] = useState([]);
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const [tableData, setTableData] = useState([]);
   let time = duration;
 
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef()
+  const originRef = useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef()
+  const destiantionRef = useRef();
 
-  console.log({distance, duration});
+  console.log({ distance, duration });
 
   if (!isLoaded) {
-    return <SkeletonText />
+    return <SkeletonText />;
   }
 
   async function calculateRoute() {
-    if (originRef.current.value === '' || destiantionRef.current.value === '') {
-      return
+    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+      return;
     }
+
     // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
       origin: originRef.current.value,
       destination: destiantionRef.current.value,
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration.text)
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+
+    setTableData((currentValue) => {
+      const spread = [
+        ...currentValue,
+        {
+          des1: originRef.current.value,
+          des2: destiantionRef.current.value,
+          timeString: results.routes[0].legs[0].duration.text,
+          timeNumber: results.routes[0].legs[0].duration.value,
+        },
+      ];
+
+      console.log({ spread });
+
+      const final = spread.sort((a, b) => a.timeNumber - b.timeNumber);
+
+      console.log({ final });
+
+      return final;
+    });
 
     // console.log({DestinoUno: originRef.current.value, DestinoDos: destiantionRef.current.value});
 
@@ -75,40 +96,40 @@ function App() {
       originRef.current.value,
       destiantionRef.current.value,
       time,
-    ]
+    ];
 
     console.log(destinationTable);
   }
 
   function clearRoute() {
-    setDirectionsResponse(null)
-    setDistance('')
-    setDuration('')
-    originRef.current.value = ''
-    destiantionRef.current.value = ''
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destiantionRef.current.value = "";
   }
 
   return (
     <Flex
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      h='100vh'
-      w='100vw'
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+      w="100vw"
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
         {/* Google Map Box */}
         <GoogleMap
           center={center}
           zoom={15}
-          mapContainerStyle={{ width: '100%', height: '100%' }}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
           options={{
             zoomControl: false,
             streetViewControl: false,
             mapTypeControl: false,
             fullscreenControl: false,
           }}
-          onLoad={map => setMap(map)}
+          onLoad={(map) => setMap(map)}
         >
           <Marker position={center} />
           {directionsResponse && (
@@ -118,50 +139,50 @@ function App() {
       </Box>
       <Box
         p={4}
-        borderRadius='lg'
+        borderRadius="lg"
         m={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='1'
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="1"
       >
-        <HStack spacing={2} justifyContent='space-between'>
+        <HStack spacing={2} justifyContent="space-between">
           <Box flexGrow={1}>
             <Autocomplete>
-              <Input type='text' placeholder='Origen' ref={originRef} />
+              <Input type="text" placeholder="Origen" ref={originRef} />
             </Autocomplete>
           </Box>
           <Box flexGrow={1}>
             <Autocomplete>
-              <Input
-                type='text'
-                placeholder='Destino'
-                ref={destiantionRef}
-              />
+              <Input type="text" placeholder="Destino" ref={destiantionRef} />
             </Autocomplete>
           </Box>
 
           <ButtonGroup>
-            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
               Calcular y añadir ruta
             </Button>
             <IconButton
-              aria-label='center back'
+              aria-label="center back"
               icon={<FaTimes />}
               onClick={clearRoute}
             />
           </ButtonGroup>
         </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distancia: <p>{distance}</p> </Text>
-          <Text>Tiempo: <p>{duration}</p> </Text>
+        <HStack spacing={4} mt={4} justifyContent="space-between">
+          <Text>
+            Distancia: <p>{distance}</p>{" "}
+          </Text>
+          <Text>
+            Tiempo: <p>{duration}</p>{" "}
+          </Text>
           <IconButton
-            aria-label='center back'
+            aria-label="center back"
             icon={<FaLocationArrow />}
             isRound
             onClick={() => {
-              map.panTo(center)
-              map.setZoom(15)
+              map.panTo(center);
+              map.setZoom(15);
             }}
           />
         </HStack>
@@ -170,37 +191,41 @@ function App() {
       <Box
         mt={600}
         p={4}
-        borderRadius='lg'
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='1'>
-          <HStack>
-            <Text>Destinos</Text>
-            <Button>Calcular</Button>
-          </HStack>
-          <TableContainer>
-            <Table variant='simple'>
-              <Thead>
-                <Th>#</Th>
-                <Th>Destino 1</Th>
-                <Th>Destino 2</Th>
-                <Th>Tiempo</Th>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>1</Td>
-                  <Td>Destino 1</Td>
-                  <Td>Destino 2</Td>
-                  <Td>Tiempo</Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
+        borderRadius="lg"
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="1"
+      >
+        <HStack>
+          <Text>Destinos</Text>
+          <Button>Calcular</Button>
+        </HStack>
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Th>#</Th>
+              <Th>Destino 1</Th>
+              <Th>Destino 2</Th>
+              <Th>Tiempo</Th>
+            </Thead>
+            <Tbody>
+              {tableData.map((value, index) => {
+                return (
+                  <Tr key={index}>
+                    <Td>{index + 1}</Td>
+                    <Td>{value.des1}</Td>
+                    <Td>{value.des2}</Td>
+                    <Td>{value.timeString}</Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Box>
-
     </Flex>
-  )
+  );
 }
 
-export default App
+export default App;
